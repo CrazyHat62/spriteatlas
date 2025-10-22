d10d8ae117fdca2834548ba70d1c496e5d497343
richard <richardporteous343@gmail.com>
2025-10-21 18:32:53 -0600

    read 1st line in atlas

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,12 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+func TestReadAtlasRow(t *testing.T) {
+
+	got, err := ReadAtlas("atiles_test.atlas")
+	want := "page \"atiles.bmp\" 255,0,255,255 true 48,48 0,0,0,0"
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q with error %q", got, want, err)
 	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println(Hello("world"))
+	line, _ := ReadAtlas("atiles.atlas")
+	println(line)
 }
 
-func Hello(name string) string {
-	return "Hello, " + name
+func ReadAtlas(name string) (string, error) {
+
+	var v []byte
+
+	file, err := os.Open(name)
+	if err != nil && err != io.EOF {
+		os.Exit(1)
+	}
+
+	var str string
+
+	defer file.Close()
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+		//remove newline and carrige return if it exists from line
+		line = bytes.ReplaceAll(line, []byte("\r"), []byte(""))
+		line = bytes.ReplaceAll(line, []byte("\n"), []byte(""))
+		//line = bytes.ReplaceAll(line, []byte(" "), []byte(""))
+		v = line
+		s := string(v)
+		s = strings.Trim(s, " ")
+		if s == "" || s[:1] == "#" || s[:2] == "//" {
+			continue
+		}
+		str = s
+		break
+	}
+
+	return str, err
+
 }
