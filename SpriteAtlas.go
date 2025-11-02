74086b10909eda84e29ed1d4bd1a898b5b79cb9b
richard <richardporteous343@gmail.com>
2025-11-01 18:50:37 -0600

    not getting correct positions yet

--- SpriteAtlas.go
+++ SpriteAtlas.go
@@ -27,10 +27,10 @@ type Anim struct {
 }
 
 type Region struct {
-	Name       string
-	RegionSize RECT
-	TileSize   XY
-	Anims      map[string]Anim
+	Name         string
+	RegionBounds RECT
+	TileSize     XY
+	Anims        map[string]Anim
 }
 
 func (r *Region) ParseRegionStr(values []string) error {
@@ -56,10 +56,10 @@ func (r *Region) ParseRegionStr(values []string) error {
 	if err != nil {
 		errstr = errstr + "Parse Region rect size Y2 failed, "
 	}
-	r.RegionSize.X1 = int(px1)
-	r.RegionSize.X2 = int(px2)
-	r.RegionSize.Y1 = int(py1)
-	r.RegionSize.Y2 = int(py2)
+	r.RegionBounds.X1 = int(px1)
+	r.RegionBounds.X2 = int(px2)
+	r.RegionBounds.Y1 = int(py1)
+	r.RegionBounds.Y2 = int(py2)
 
 	t := strings.Split(values[2], ",")
 	tx, err := strconv.ParseInt(t[0], 0, 0)
@@ -119,7 +119,7 @@ type Page struct {
 	name             string
 	alpha_color      string
 	imageRegionMarks bool
-	tile_size        XY
+	sheetSize        XY
 	margin           RECT
 }
 
@@ -144,8 +144,8 @@ func (p *Page) ParsePageStr(values []string) error {
 		errstr = errstr + "Parse Atlas tile size Y failed, "
 	}
 
-	p.tile_size.X = int(px)
-	p.tile_size.Y = int(py)
+	p.sheetSize.X = int(px)
+	p.sheetSize.Y = int(py)
 	mx1, err := strconv.ParseInt(m[0], 0, 0)
 	if err != nil {
 		errstr = errstr + "Parse Atlas margin X1 failed, "
@@ -174,7 +174,7 @@ func (p *Page) ParsePageStr(values []string) error {
 }
 
 func (p *Page) PageToStr() string {
-	return fmt.Sprintf("page %s has default tilesize %d %d", p.name, p.tile_size.X, p.tile_size.Y)
+	return fmt.Sprintf("page %s has alphacolor=%s and sheetSize %d %d with margins %d,%d,%d,%d", p.name, p.alpha_color, p.sheetSize.X, p.sheetSize.Y, p.margin.X1, p.margin.Y1, p.margin.X2, p.margin.Y2)
 }
 
 var page Page
@@ -236,4 +236,32 @@ func StripAtlasLine(line []byte) string {
 	s := string(line)
 	s = strings.Trim(s, " ")
 	return s
+}
+
+// GetAnimation gets the RECT for the given animation name and frame index in that animation
+func GetAnimation(region *Region, animName string, idx int) (RECT, error) {
+	anim, ok := region.Anims[animName]
+	var rect RECT
+
+	if !ok {
+		return rect, errors.New("animation %q not found in region " + region.Name)
+	}
+	// get correct frame number in animation
+	idx = idx % anim.Count
+
+	//rectangle within region for 1st frame + plus region offset
+	rect.X1 = (anim.Pos0.X-1)*region.TileSize.X + region.RegionBounds.X1
+	rect.Y1 = (anim.Pos0.Y-1)*region.TileSize.Y + region.RegionBounds.Y1
+	//calculate start position of frame within animation
+	startPosX := idx * region.TileSize.X
+	rect.X1 += startPosX
+	rect.X2 = rect.X1 + region.TileSize.X
+	rect.Y2 = rect.Y2 + region.TileSize.Y
+
+	// idxOffsetX := idx*region.TileSize.X
+	// //
+	// startPosX := (anim.Pos0.X * idx) * region.TileSize.X
+	// startPosY := (anim.Pos0.Y - 1) * region.TileSize.Y
+
+	return rect, nil
 }
