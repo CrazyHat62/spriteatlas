57afb78db23ddf93ec8c5b80dc99f460a9e0f10c
richard <richardporteous343@gmail.com>
2025-11-03 14:47:49 -0700

    believe get animation now works

--- SpriteAtlas.go
+++ SpriteAtlas.go
@@ -15,10 +15,14 @@ type XY struct {
 }
 
 type RECT struct {
-	X1 int
-	Y1 int
-	X2 int
-	Y2 int
+	X      int
+	Y      int
+	Width  int
+	Height int
+}
+
+func (r *RECT) RectToStr() string {
+	return fmt.Sprintf("rect is at pos %v, %v with size %v, %v", r.X, r.Y, r.Width, r.Height)
 }
 
 type Anim struct {
@@ -27,10 +31,10 @@ type Anim struct {
 }
 
 type Region struct {
-	Name         string
-	RegionBounds RECT
-	TileSize     XY
-	Anims        map[string]Anim
+	Name     string
+	Bounds   RECT
+	TileSize XY
+	Anims    map[string]Anim
 }
 
 func (r *Region) ParseRegionStr(values []string) error {
@@ -56,10 +60,10 @@ func (r *Region) ParseRegionStr(values []string) error {
 	if err != nil {
 		errstr = errstr + "Parse Region rect size Y2 failed, "
 	}
-	r.RegionBounds.X1 = int(px1)
-	r.RegionBounds.X2 = int(px2)
-	r.RegionBounds.Y1 = int(py1)
-	r.RegionBounds.Y2 = int(py2)
+	r.Bounds.X = int(px1)
+	r.Bounds.Width = int(px2)
+	r.Bounds.Y = int(py1)
+	r.Bounds.Height = int(py2)
 
 	t := strings.Split(values[2], ",")
 	tx, err := strconv.ParseInt(t[0], 0, 0)
@@ -100,6 +104,30 @@ func (r *Region) ParseRegionStr(values []string) error {
 		return errors.New(errstr)
 	}
 	return nil
+}
+
+// GetAnimation gets the RECT for the given animation name and frame index in that animation
+func (r *Region) GetAnimation(animName string, idx int) (RECT, error) {
+	anim, ok := r.Anims[animName]
+	var rect RECT
+	if !ok {
+		return rect, errors.New("animation %q not found in region " + r.Name)
+	}
+
+	rect = RECT{X: 0, Y: 0, Width: r.TileSize.X, Height: r.TileSize.Y}
+	nextFrame := 0
+	idx = idx % anim.Count
+	if idx > 0 {
+		nextFrame = 1
+	}
+
+	offsetX := (anim.Pos0.X - 1) * region.TileSize.X
+	offsetY := (anim.Pos0.Y - 1) * region.TileSize.Y
+
+	rect.X = idx*region.TileSize.X + nextFrame + offsetX + region.Bounds.X
+	rect.Y = offsetY + region.Bounds.Y
+
+	return rect, nil
 }
 
 func (r *Region) RegionToStr() string {
@@ -162,10 +190,10 @@ func (p *Page) ParsePageStr(values []string) error {
 	if err != nil {
 		errstr = errstr + "Parse Atlas margin Y2 failed, "
 	}
-	p.margin.X1 = int(mx1)
-	p.margin.X2 = int(mx2)
-	p.margin.Y1 = int(my1)
-	p.margin.Y2 = int(my2)
+	p.margin.X = int(mx1)
+	p.margin.Width = int(mx2)
+	p.margin.Y = int(my1)
+	p.margin.Height = int(my2)
 
 	if errstr != "" {
 		return errors.New(errstr)
@@ -174,7 +202,7 @@ func (p *Page) ParsePageStr(values []string) error {
 }
 
 func (p *Page) PageToStr() string {
-	return fmt.Sprintf("page %s has alphacolor=%s and sheetSize %d %d with margins %d,%d,%d,%d", p.name, p.alpha_color, p.sheetSize.X, p.sheetSize.Y, p.margin.X1, p.margin.Y1, p.margin.X2, p.margin.Y2)
+	return fmt.Sprintf("page %s has alphacolor=%s and sheetSize %d %d with margins %d,%d,%d,%d", p.name, p.alpha_color, p.sheetSize.X, p.sheetSize.Y, p.margin.X, p.margin.Y, p.margin.Width, p.margin.Height)
 }
 
 var page Page
@@ -236,32 +264,4 @@ func StripAtlasLine(line []byte) string {
 	s := string(line)
 	s = strings.Trim(s, " ")
 	return s
-}
-
-// GetAnimation gets the RECT for the given animation name and frame index in that animation
-func GetAnimation(region *Region, animName string, idx int) (RECT, error) {
-	anim, ok := region.Anims[animName]
-	var rect RECT
-
-	if !ok {
-		return rect, errors.New("animation %q not found in region " + region.Name)
-	}
-	// get correct frame number in animation
-	idx = idx % anim.Count
-
-	//rectangle within region for 1st frame + plus region offset
-	rect.X1 = (anim.Pos0.X-1)*region.TileSize.X + region.RegionBounds.X1
-	rect.Y1 = (anim.Pos0.Y-1)*region.TileSize.Y + region.RegionBounds.Y1
-	//calculate start position of frame within animation
-	startPosX := idx * region.TileSize.X
-	rect.X1 += startPosX
-	rect.X2 = rect.X1 + region.TileSize.X
-	rect.Y2 = rect.Y2 + region.TileSize.Y
-
-	// idxOffsetX := idx*region.TileSize.X
-	// //
-	// startPosX := (anim.Pos0.X * idx) * region.TileSize.X
-	// startPosY := (anim.Pos0.Y - 1) * region.TileSize.Y
-
-	return rect, nil
 }
--- SpriteAtlas_test.go
+++ SpriteAtlas_test.go
@@ -12,7 +12,7 @@ func TestOpenAtlas(t *testing.T) {
 	if err != nil {
 		t.Errorf("got open file error %q", err)
 	}
-	want := "page atiles.bmp has default tilesize 48 48. region player_walk has 4 animations"
+	want := "page atiles.bmp has alphacolor=255,0,255,255 and sheetSize 1729 874 with margins 1,1,1,1. region player_walk has 4 animations"
 	got := page.PageToStr() + ". " + region.RegionToStr()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
@@ -33,7 +33,7 @@ func TestParsePageStr(t *testing.T) {
 	var page Page
 	err := page.ParsePageStr([]string{"atiles.bmp", "255,0,255,255", "true", "48,48", "0,0,0,0"})
 	got := page.PageToStr()
-	want := "page atiles.bmp has default tilesize 48 48"
+	want := "page atiles.bmp has alphacolor=255,0,255,255 and sheetSize 48 48 with margins 0,0,0,0"
 	if got != want {
 		t.Errorf("got %q want %q with error %q", got, want, err)
 	}
@@ -63,6 +63,55 @@ func TestAnimKeys(t *testing.T) {
 		}
 	}
 }
+
+// func TestGetAnimation(t *testing.T) {
+
+// 	var reg Region
+// 	err := reg.ParseRegionStr([]string{"player_walk", "1,148,384,244", "47,47", "north,1,1,4", "west,1,5,4", "south,2,1,4", "east,2,5,4"})
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	got1, err := reg.GetAnimation("north", 0)
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	got2, err := reg.GetAnimation("north", 1)
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	got3, err := reg.GetAnimation("north", 2)
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	got4, err := reg.GetAnimation("north", 3)
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	got5, err := reg.GetAnimation("north", 4)
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	want1 := RECT{X: 1, Y: 148, Width: 47, Height: 47}
+// 	want2 := RECT{X: 48, Y: 148, Width: 47, Height: 47}
+// 	want3 := RECT{X: 95, Y: 148, Width: 47, Height: 47}
+// 	want4 := RECT{X: 142, Y: 148, Width: 47, Height: 47}
+// 	want5 := RECT{X: 1, Y: 148, Width: 47, Height: 47}
+// 	if got1 != want1 {
+// 		t.Errorf("got %v want %v", got1, want1)
+// 	}
+// 	if got2 != want2 {
+// 		t.Errorf("got %v want %v", got2, want2)
+// 	}
+// 	if got3 != want3 {
+// 		t.Errorf("got %v want %v", got3, want3)
+// 	}
+// 	if got4 != want4 {
+// 		t.Errorf("got %v want %v", got4, want4)
+// 	}
+// 	if got5 != want5 {
+// 		t.Errorf("got %v want %v", got5, want5)
+// 	}
+// }
 
 func ExampleStripAtlasLine() {
 	// the last comment actualy causes this to be run ~ not just compiler tested
