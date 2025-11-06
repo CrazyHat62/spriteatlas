bf384fea40de76c5bd2a0130866349d9a9143117
richard <richardporteous343@gmail.com>
2025-11-06 12:21:38 -0700

    comment documentation kinda

--- SpriteAtlas_test.go
+++ SpriteAtlas_test.go
@@ -1,3 +1,12 @@
+// Package spriteatlas tests.
+// tests various functions to pass.
+//
+//	missing:
+//	1. range outside image(page)
+//	2. frame outside range
+//	3. plus other limit tests
+//
+// tests are basic and need improvement
 package spriteatlas
 
 import (
@@ -6,58 +15,63 @@ import (
 )
 
 var (
-	pageAns1    = "page atiles.bmp has alphacolor=255,0,255,255 and sheetSize 1729 874 with margins 1,1,1,1. region player_walk has 4 animations"
-	pageAns2    = "page atiles.bmp 255,0,255,255 true 48,48 0,0,0,0"
-	pageAns3    = "page atiles.bmp has alphacolor=255,0,255,255 and sheetSize 48 48 with margins 0,0,0,0"
-	pageStr1    = "page atiles.bmp 255,0,255,255 true 48,48 0,0,0,0\n\r"
-	pageStrArr1 = []string{"atiles.bmp", "255,0,255,255", "true", "48,48", "0,0,0,0"}
-	regionStr1  = []string{"player_walk", "1,148,384,243", "48,48", "north,1,1,4", "west,5,1,4", "south,1,2,4", "east,5,2,4"}
+	pageStr1Ans1    = "page atiles.bmp has alphacolor=255,0,255,255 and sheetSize 1729 874 with margins 1,1,1,1. region player_walk has 4 animations"
+	pageStr1Ans2    = "page atiles.bmp 255,0,255,255 true 48,48 0,0,0,0"
+	pageStrArr1Ans3 = "page atiles.bmp has alphacolor=255,0,255,255 and sheetSize 48 48 with margins 0,0,0,0"
+	pageStr1        = "page atiles.bmp 255,0,255,255 true 48,48 0,0,0,0\n\r"
+	pageStrArr1     = []string{"atiles.bmp", "255,0,255,255", "true", "48,48", "0,0,0,0"}
+	regionStr1      = []string{"player_walk", "1,148,384,243", "48,48", "north,1,1,4", "west,5,1,4", "south,1,2,4", "east,5,2,4"}
+	regionStr1Ans1  = "region player_walk has 4 animations"
 )
 
-// TestReadAtlasRow is a test to verify file open and a general Parse the of specific atlas
+// TestReadAtlasRow is a test to verify file open and a Parse the of a test atlas
 func TestOpenAtlas(t *testing.T) {
 
 	page, region, err := Spriteatlas("", "atiles_test.atlas")
 	if err != nil {
 		t.Errorf("got open file error %q", err)
 	}
-	want := pageAns1
+	want := pageStr1Ans1
 	got := page.PageToStr() + ". " + region.RegionToStr()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
 
+// TestStripAtlasLine tests to see if line ends are removed before placing in a string
 func TestStripAtlasLine(t *testing.T) {
 	b := []byte(pageStr1)
 	got := StripAtlasLine(b)
 
-	want := pageAns2
+	want := pageStr1Ans2
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
 
+// TestParsePageStr - tests the Page line in the atlas
 func TestParsePageStr(t *testing.T) {
 	var page Page
 	err := page.ParsePageStr(pageStrArr1)
 	got := page.PageToStr()
-	want := pageAns3
+	want := pageStrArr1Ans3
 	if got != want {
 		t.Errorf("got %q want %q with error %q", got, want, err)
 	}
 }
 
+// TestParseRegionStr - tests the Region line in the atlas
 func TestParseRegionStr(t *testing.T) {
 	var reg Region
 	err := reg.ParseRegionStr(regionStr1)
 	got := reg.RegionToStr()
-	want := "region player_walk has 4 animations"
+	want := regionStr1Ans1
 	if got != want {
 		t.Errorf("got %q want %q with error %q", got, want, err)
 	}
 }
 
+// TestAnimKeys - tests if only expected items exist - write another to check if all items exist
 func TestAnimKeys(t *testing.T) {
 	var reg Region
 	err := reg.ParseRegionStr(regionStr1)
@@ -73,6 +87,7 @@ func TestAnimKeys(t *testing.T) {
 	}
 }
 
+// TestGetAnimation - tests to see if we get the expected frame (rect x,y,w,h) for the image
 func TestGetAnimation(t *testing.T) {
 
 	var reg Region
@@ -189,6 +204,7 @@ func TestGetAnimation(t *testing.T) {
 
 }
 
+// ExampleStripAtlasLine ~~ same as ~~ TestStripAtlasLine, but written differently
 func ExampleStripAtlasLine() {
 	// the last comment actualy causes this to be run ~ not just compiler tested
 	b := []byte("abc\n\r")
